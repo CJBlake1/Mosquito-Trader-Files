9cc61bc7639b2c3f84b3084ff7c95ca15454f56b
CJB <sblake93@yahoo.com>
2025-05-12 13:19:07 -0400

    Add files via upload

---
+++ Mosquito/MOSQUITO/ItemListMOSQUITO.hpp
@@ -0,0 +1,13 @@
+	///////////////////////////////////////////////////////////////////////////////
+	//MOSQUITO
+	///////////////////////////////////////////////////////////////////////////////
+	class RwG_Mozzie_Carl_Red					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Blue					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Green				{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Pink					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Yellow					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Black					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_White					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Orange					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Gold					{ quality = 1; price = 5000; };
+	class RwG_Mozzie_Carl_Exile					{ quality = 1; price = 5000; };
---
+++ Mosquito/MOSQUITO/TraderCategoriesMOSQUITO.hpp
@@ -0,0 +1,18 @@
+class MOSQUITO
+	{
+		name = "MOSQUITO";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+			"RwG_Mozzie_Carl_Red",
+			"RwG_Mozzie_Carl_Blue",
+			"RwG_Mozzie_Carl_Green",
+			"RwG_Mozzie_Carl_Pink",
+			"RwG_Mozzie_Carl_Yellow",
+			"RwG_Mozzie_Carl_Black",
+			"RwG_Mozzie_Carl_White",
+			"RwG_Mozzie_Carl_Orange",
+			"RwG_Mozzie_Carl_Gold",
+			"RwG_Mozzie_Carl_Exile"		
+		};
+	};
